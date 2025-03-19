9d0a2dd06e2d4bf4684d0bff7dd32f875086dc18
adrianvarnier <avarnier@Arcadia.>
2025-03-19 02:22:54 +0100

    update --help

--- display.c
+++ display.c
@@ -3,10 +3,15 @@
 void    display_help()
 {
     printf("Usage:\n");
-    printf("\tft_traceroute host\n");
+    printf("\tft_traceroute [options] host\n");
     printf("\n");
     printf("Options:\n");
-    printf("\t--help:\t\tRead this help and exit\n");
+    printf("\t--help\t\tRead this help and exit\n");
+    printf("\t-I\t\tUse ICMP ECHO for probes\n");
+    printf("\t-f first_ttl\tSpecifies with what TTL to start. Defaults to 1.\n");
+    printf("\t-m max_ttl\tSpecifies the maximum number of hops (max time-to-live value) traceroute will probe. The default is 30.\n");
+    printf("\t-w waittime\tSet the time (in seconds) to wait for a response to a probe (default 5.0 sec).\n");
+    printf("\t-q nqueries\tSets the number of probe packets per hop. The default is 3.\n");
     printf("\n");
     printf("Arguments:\n");
     printf("\thost:\t\tThe host to traceroute to\n");
